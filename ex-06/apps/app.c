cd7f6c2ba0399565bcba30c459efad9d666a62dd
luckera <lucasromero1702@gmail.com>
2022-09-25 16:56:09 -0300

    Adição do ex-06

---
+++ ex-06/apps/app.c
@@ -0,0 +1,15 @@
+#include "linked_list.h"
+#include <stdio.h>
+
+int main(){
+    
+    int k; // Número de Filas
+
+    scanf("%d", &k);
+
+    for(int i = 0; i < k; i++){
+        
+    }
+
+    return 0;
+}
---
+++ ex-06/include/linked_list.h
@@ -0,0 +1,16 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+//*************************** HEADER DO TAD - ESPECIFICAÇÃO ***************************//
+
+typedef struct _snode SNode;
+typedef struct _linked_list LinkedList;
+
+// Assinatura das Funções do TAD
+
+
+
+
+//*************************************************************************************//
+
+#endif
