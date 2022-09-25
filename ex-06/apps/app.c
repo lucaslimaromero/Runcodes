f58cc0320f1bdcb38ef2b6b241090fa9e1df94aa
luckera <lucasromero1702@gmail.com>
2022-09-25 20:04:06 -0300

    Commit 6

--- ex-06/apps/app.c
+++ ex-06/apps/app.c
@@ -7,9 +7,31 @@ int main(){
 
     scanf("%d", &k);
 
+    LinkedList **L_vec = LinkedList_vector_create(k); // Criação do vetor de listas
+
     for(int i = 0; i < k; i++){
-        
+        LinkedList_read(L_vec[i]);
     }
+
+    // É necessário criar uma forma de organizar esse vetor de listas conforme a ordem que devem ser printados
+
+    for(int i = 0; i < k; i++){
+        for(int j = 0; j < k - 1; j++){
+            
+        }
+    }
+
+    printf("Fila Unica:\n");
+    for(int i = 0; i < k; i++){
+        LinkedList_print(L_vec[i]);
+    }
+
+    // Desalocando as Listas
+
+    // for(int i = 0; i < k; i++){
+    //     LinkedList_destroy(L_vec[i]);
+    // }
+    
 
     return 0;
 }
